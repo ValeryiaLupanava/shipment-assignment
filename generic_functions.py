db5b769c17a6d854e669e48ddad76196be2ef19a
ValeryiaLupanava <ValeryiaLupanava>
2024-07-21 09:37:23 +0000

    fix

--- generic_functions.py
+++
@@ -1,6 +0,0 @@
-def read_data_from_db(db_connection: str, connection_properties:table_schema: str, table_name: str, filter: str="1=1"):
-    """ Reading data from database """
-    connection_properties = {"driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"}
-    input_qry = f"""(SELECT * FROM {table_schema}.{table_name} where {filter}) tbl"""
-    result_df = spark.read.jdbc(db_connection, input_qry, properties=connection_properties)
-    return result_df
