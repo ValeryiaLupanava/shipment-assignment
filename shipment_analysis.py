3903f983740c977184c7db65b074d9c5b121a80e
ValeryiaLupanava <ValeryiaLupanava>
2024-07-21 09:37:02 +0000

    fix

--- shipment_analysis.py
+++ shipment_analysis.py
@@ -1,19 +1,13 @@
 # Databricks notebook source
-from generic_functions import read_data_from_db
-
-# DB connection
-#secret_scope = "sc-shipments"
-#db_connection_key = dbutils.secrets.get(scope=secret_scope, key=db_connection_key) 
-db_connection_key = ""
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate("shipment_analysis")
 
 # COMMAND ----------
 
-# Reading shipment information
-shipments_df = read_data_from_db(db_connection=db_connection_key, table_schema="dbo", table_name="shipments", filter="1=1")
-shipments_df.display()
+# Reading shipment and logger information
+shipments_df = spark.table("shipments.shipments")
+logger_temperature_df = spark.table("shipments.logger_temperature")
 
 # COMMAND ----------
 
-# Reading logger information
-logger_df = read_data_from_db(db_connection=db_connection_key, table_schema="dbo", table_name="logger_temperature", filter="1=1")
-logger_df.display()
+
